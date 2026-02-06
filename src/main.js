d241a74162845d323fa58be2570bbec268bce13c
Ralitnyi Ivan <ralitniyivan@gmail.com>
2026-02-06 11:14:16 +0200

    fix: update main.js and remove unused favicon

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './js/mobile-menu.js';
+import './js/ui/quote';
+import './js/ui/filters';
+import './js/ui/exercises';
+import './js/ui/modalExercises';
+import './js/ui/rating';
+import './js/ui/search';
+import './js/ui/pagination';
+import './js/ui/subscribe';
+import './js/ui/favorites';
+import './js/ui/header';
